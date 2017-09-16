d30c10da8646c673f5eeae39d058ceb6960919d5
quinnclark623 <quinnclark623@gmail.com>
2017-09-15 18:52:37 -0600

    Add files via upload
    these are the three files for the part 2 of the homework

---
+++ Number.cpp
@@ -0,0 +1,8 @@
+#include "Number.h"
+// the #include tag is needed here as is always in headers and source
+
+double area(double x)
+{
+
+return x;
+}
---
+++ Number.h
@@ -0,0 +1,11 @@
+
+//for header files we put our classes
+
+
+//this will be a simple program
+class Number
+{
+    double area(double x)
+    {
+    }
+};
